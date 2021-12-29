2ee816055ac2383ebe7b89123df8ccbb6a9252d2
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2021-12-29 15:56:51 +0500

    Fixing Firebase

--- app/src/main/java/com/example/mad_lab_exam/Firebase.kt
+++ app/src/main/java/com/example/mad_lab_exam/Firebase.kt
@@ -1,14 +1,22 @@
 package com.example.mad_lab_exam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_firebase.*
+import java.time.LocalDate
 
 class Firebase : AppCompatActivity() {
     val TAG = "TEST TAG"
+    private var auth: FirebaseAuth = Firebase.auth
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase)
@@ -38,15 +46,13 @@ class Firebase : AppCompatActivity() {
                         saveToFirestore(
                             etEmail.trim(),
                             etUserName.trim(),
-                            etNumber.trim()
                         )
                     } else {
-                        _progressBar.visibility = View.INVISIBLE
                         // If sign in fails, display a message to the user.
                         Log.w(TAG, "createUserWithEmail:failure", task.exception)
                         Toast.makeText(
                             baseContext,
-                            AUTHENTICATION_FAILED_ERROR + (task.getException()?.message
+                            "AUTHENTICATION_FAILED_ERROR" + (task.getException()?.message
                                 ?: ""),
                             Toast.LENGTH_SHORT
                         ).show()
@@ -54,8 +60,29 @@ class Firebase : AppCompatActivity() {
                 }
 
         } else {
-            _progressBar.visibility = View.INVISIBLE
-            Toast.makeText(this, EMPTY_FIELDS_ERROR, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "EMPTY_FIELDS_ERROR", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveToFirestore(userEmail: String, userName: String) {
+        val user = hashMapOf(
+            "email" to userEmail,
+            "username" to userName
+        )
+        db.collection("users").add(user).addOnSuccessListener { documentReference ->
+            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            Toast.makeText(
+                baseContext,
+                "Account Created, You can search a username.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }.addOnFailureListener { e ->
+            Log.w(TAG, "Error adding document", e)
+            Toast.makeText(
+                baseContext,
+                "Error" + e.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
